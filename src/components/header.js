f14bc096906c1f9d5e8ff1843e5c978b239ce9ca
Daniel <daniel@Developers-MacBook-Pro-3.local>
2019-09-06 10:40:39 -0400

    mobile layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,30 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState} from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle }) => {
+  const [isActive, setIsActive] = useState(false)
+  return(
   <nav
     className="navbar is-info"
     role="navigation"
-    ariaLabel="dropdown navigation"
+    aria-label="dropdown navigation"
   >
-    <div className="navbar-menu">
+    <div className="navbar-brand">
+    <Link className="navbar-item" to="/">
+      Closure Templates
+    </Link>
+      <span role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" onClick={() => setIsActive(!isActive)}>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </span>
+    </div>
+    <div className={`navbar-menu ${isActive ? "is-active": ''}`}>
       <div className="navbar-start">
         <div className="navbar-item has-dropdown is-hoverable">
           <Link className="navbar-link" to="/">
-            {siteTitle}
+            Documentation
           </Link>
           <div className="navbar-dropdown">
             <Link className="navbar-item" to="/docs/concepts">
@@ -29,7 +41,7 @@ const Header = ({ siteTitle }) => (
       </div>
     </div>
   </nav>
-)
+)}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
